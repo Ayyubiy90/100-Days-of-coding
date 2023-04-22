6d8840ff1342e82b228a7f8e25fb22f1f23a2abe
ayb <ayyubiy67@gmail.com>
2023-04-22 14:29:53 +0300

    started NodeJs and still on introduction Learnt how to create a sever using NodeJs

--- NodeJs/app.js
+++ NodeJs/app.js
@@ -1,8 +1,18 @@
-const http = require('http');
+const http = require("http");
 
-const server = http.createServer();
+function handleRequest(request, response) {
+  if (request.url === "/currenttime") {
+    response.statusCode = 200;
+    response.end("<h1>" + new Date().toISOString() + "</h1>");
+  } else if (request.url === "/") {
+    response.statusCode = 200;
+    response.end("<h1>Hello World!</h1>");
+  }
+}
+
+const server = http.createServer(handleRequest);
 
 server.listen(3000);
 
 // amazon.com => send request to Amazon's server
-// amazon.com:80 or 443 are the defaults... 
+// amazon.com:80 or 443 are the defaults...
