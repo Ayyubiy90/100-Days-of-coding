f0de925717e53ffa6430776e5ee62e8876bdf47c
ayb <ayyubiy67@gmail.com>
2023-04-25 16:49:59 +0300

    adding EJS template engine to my codes changing HTML file to .EJS since EJS has been added to my code

--- Front End Site/app.js
+++ Front End Site/app.js
@@ -1,33 +1,48 @@
+const fs = require("fs");
 const path = require("path");
 
 const express = require("express");
 
 const app = express();
 
-app.use(express);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>");
-});
-
-app.get("/", function (req, res) {
-  const htmlFilePath = path.join(__dirname, "views", "index.html");
-  res.sendFile(htmlFilePath);
+  res.render("index");
 });
 
 app.get("/restaurants", function (req, res) {
-    const htmlFilePath = path.join(__dirname, "views", "restaurants.html");
-    res.sendFile(htmlFilePath); 
-  });
-  
-  app.get("/recommend", function (req, res) {
-    const htmlFilePath = path.join(__dirname, "views", "recommend.html");
-    res.sendFile(htmlFilePath);
-  });
+  res.render("restaurants");
+});
 
-  app.get("/about", function (req, res) {
-    const htmlFilePath = path.join(__dirname, "views", "about.html");
-    res.sendFile(htmlFilePath);
-  });
-  
+app.get("/recommend", function (req, res) {
+  res.render("recommend");
+});
+
+app.post("/recommend", function (req, res) {
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  storedRestaurants.push(restaurant);
+
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+  res.redirect("/confirm");
+});
+
+app.get("/confirm", function (req, res) {
+  res.render("confirm");
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
 app.listen(3000);
