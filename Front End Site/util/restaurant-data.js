0a30a05ebd807004d3f59032fcdd5fdaaaa58129
ayb <ayyubiy67@gmail.com>
2023-04-27 18:23:02 +0300

    importing & exporting code in NodeJs

--- Front End Site/util/restaurant-data.js
+++ Front End Site/util/restaurant-data.js
@@ -1,7 +1,9 @@
-const filePath = path.join(__dirname, "data", "restaurants.json");
+const path = require("path");
+const fs = require("fs");
+
+const filePath = path.join(__dirname, "..", "data", "restaurants.json");
 
 function getStoredRestaurants() {
-
   const fileData = fs.readFileSync(filePath);
   const storedRestaurants = JSON.parse(fileData);
 
@@ -9,5 +11,10 @@ function getStoredRestaurants() {
 }
 
 function storeRestaurants(storableRestaurants) {
-    fs.writeFileSync(filePath, JSON.stringify(storableRestaurants));
+  fs.writeFileSync(filePath, JSON.stringify(storableRestaurants));
 }
+
+module.exports = {
+  getStoredRestaurants: getStoredRestaurants,
+  storeRestaurants: storeRestaurants,
+};
